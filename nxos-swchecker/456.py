e65e3a707b16c74373b1b6e9ec112013963b4d64
Jason Marter <jason.marter@gmail.com>
2018-02-01 12:33:54 -0600

    Update 456.py

--- nxos-swchecker/456.py
+++ nxos-swchecker/456.py
@@ -1,8 +1,9 @@
-import subprocess
-import os
+
+
 from ssh import ssh
 from ip_list_class import ListIP
 from ip_list_class import *
+import getpass, time, os, subprocess
 
 
 def get_router_items(string):
@@ -26,9 +27,6 @@ def router_login(login_details, command):
     except:
         pass
 
-
-
-
 def ping_ip(ip):
     res = subprocess.call(['ping', '-c', '1', ip], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     if res == 0:
@@ -37,48 +35,29 @@ def ping_ip(ip):
         return 'down'
 
 
-command = 'sh int desc'
-username = 'jmarter'
-password = 'asdasdasdas'
+#username = getpass.getuser()
+username = input('Please type your username: \n')
+password = getpass.getpass()
+time.sleep(1)
+command = input('Please type a command: \n')
+site_local = input('Please type the site lcoation: \n')
+ip_range = input("Please type a range of IP's (172.16.109.0/27, etc): \n")
 
 
 
+#joliet_ips = ListIP('172.16.109.0/27')
 
-#command = 'show interfaces status'
-
-
-# stpetersburg_ips = ListIP('172.16.224.0/27')
-# dunn_ips = ListIP('172.16.235.0/27')
-# richfield_ips = ListIP('172.16.228.0/27')
-# millington_ips = ListIP('172.16.238.0/27')
-# bethlehem_ips = ListIP('172.16.234.0/27')
-
-# tampa_ips = ListIP('172.16.225.0/27')
-
-# jacksonville_ips = ListIP('172.16.237.0/27')
-# orlando_ips = ListIP('172.16.236.0/27')
-
-joliet_ips = ListIP('172.16.109.0/27')
+site_ips = ListIP(ip_range)
 
 
 
-# site_dict = {'St Petersbug, FL': stpetersburg_ips,'Dunn, NC' : dunn_ips,'Richfield, OH' : richfield_ips,'Millington, TN' : millington_ips,'Bethlehem, PA' : bethlehem_ips,'Tampa, FL' : tampa_ips,'Jacksonville, FL' : jacksonville_ips,'Orlando, FL' : orlando_ips}
-
-site_dict = {'joliet, IL': joliet_ips}
+site_dict = {site_local : site_ips}
 
 output = set()
 for city,router_ip in site_dict.items():
-
-#for city,rtr_ip in site_dict.items():
-
-
-     print(site_dict.items())
-
-     #for rtr_ip, sw_ip in zip(router_ip.router_ips(), router_ip.switch_ips()):
      for rtr_ip in router_ip.list_ips():
          rtr_ping = ping_ip(rtr_ip)
-
-    
+  
          if rtr_ping == 'up':
     
              print('# {}'.format(rtr_ip))
